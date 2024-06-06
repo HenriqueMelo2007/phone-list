bfa77ab7fb945c5e0f1d5e68d38bc78b4b3d9576
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2024-06-06 20:48:11 -0300

    feat: it adds the code base

--- main.c
+++ main.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char const *argv[])
+int main(void)
 {
-  
+  FILE *list = fopen("phone-list.csv", "a+");
+  if (list == NULL) {
+    return 1;
+  }
+
+  char name[30];
+  char number[30];
+
+  printf("Name: ");
+  fgets(name, sizeof(name), stdin);
+  name[strcspn(name, "\n")] = '\0';  // Remove newline character
+
+  printf("Number: ");
+  fgets(number, sizeof(number), stdin);
+  number[strcspn(number, "\n")] = '\0';  // Remove newline character
+
+  fprintf(list, "%s,%s\n", name, number);
+
+  fclose(list);
+
   return 0;
 }
+
